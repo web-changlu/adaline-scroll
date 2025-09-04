3895967239bec75823f86b08b46d5c55211f9d76
web-changlu <1936787441@qq.com>
2025-09-04 16:04:13 +0800

    添加路径

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 如果你的仓库名是 adaline-scroll，请保留该 base；如果不同，请换成 '/<repo>/'
-  // base: '/adaline-scroll/',
+  base: '/adaline-scroll/',
   plugins: [vue()],
 })
